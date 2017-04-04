50170661e6e261106746adc42f440329bc3ccf38
Ruhollah Shemirani <Roohy@Ruhollahs-MacBook-Pro.local>
2017-04-04 15:59:04 -0700

    fixed include error

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+wget http://www.cs.columbia.edu/~gusev/germline/germline-1-5-1.tar.gz
+
+tar -xvzf germline-1-5-1.tar.gz
+
+sudo apt-get update
+
+sudo apt-get install cmake
+
+sudo apt-get install g++
--- writer.cpp
+++ writer.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <thread>
+#include <algorithm>
 
 using namespace std;
 
