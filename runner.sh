b7e8bb1826823f7fa3287f840007d03b1995470d
Ruhollah Shemirani <Roohy@Ruhollahs-MacBook-Pro.local>
2017-06-02 18:45:52 -0700

    shelll

---
+++ runner.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+clear
+echo "Hello $USER"
+echo "Today is \c ";date
+
+EXEC=ADDR_FOR_LSH
+TEST_INPUT_PRE=TEST
+TEST_TEMPALTE=./test_tmp
+INPUT_PRE=PAGEII_Chr
+INPUT_SUF_MAP=.phased.filtered.map3
+INPUT_SUF_PED=.phased.filtered.ped
+OUTPUT_BASE=result
+LOG_BASE=LOG
+
+for(( i = 1 ; i <= 2 ; i++))
+do
+#echo "map $INPUT_PRE$i$INPUT_SUF_MAP"
+#echo "ped $INPUT_PRE$i$INPUT_SUF_PED"
+test_addr="$TEST_INPUT_PRE$i"
+echo $test_addr
+cat $TEST_TEMPALTE > $test_addr
+echo "\nmap $INPUT_PRE$i$INPUT_SUF_MAP" >> $test_addr
+echo "ped $INPUT_PRE$i$INPUT_SUF_PED" >> $test_addr
+echo "out $OUTPUT_BASE$i" >> $test_addr
+time $EXEC $test_addr > $LOG_BASE$i
+done
+
+echo Everything is "done" successfully
